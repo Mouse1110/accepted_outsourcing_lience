603b983aee66502d394414cf2ad8e0854919baaa
Bá Nguyễn <54467503+Mouse1110@users.noreply.github.com>
2023-04-08 07:29:20 -0700

    log

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 2005
 app.use(cors())
 
 app.get('/farmext', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  var d = new Date(); 
+  console.log("===",d.toLocaleString(),"",ip,":::","Truy cập ứng dụng farmext")
   res.send({code:"000",data:"Bạn được quyền truy cập"})
 })
 
